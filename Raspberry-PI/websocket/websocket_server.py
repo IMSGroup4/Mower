68303c1124db2962cae01c5caf37ffc9da60e091
arke19 <arke19me@student.ju.se>
2023-05-03 17:29:26 +0200

    mockserver testscriptand websocket to app communication works

--- Raspberry-PI/websocket/websocket_server.py
+++ Raspberry-PI/websocket/websocket_server.py
@@ -4,16 +4,29 @@ import asyncio
 import random
 import time
 
+message_joystick =[{"action":"joystick","x":0,"y":1,"timestamp":time.time()},
+{"action":"joystick","x":0.5,"y":1,"timestamp":time.time()},
+{"action":"joystick","x":0.5,"y":1,"timestamp":time.time()},
+{"action":"joystick","x":0,"y":1,"timestamp":time.time()},
+{"action":"joystick","x":-0.5,"y":1,"timestamp":time.time()},
+{"action":"joystick","x":0,"y":1,"timestamp":time.time()},
+{"action":"joystick","x":0,"y":0,"timestamp":time.time()},
+{"action":"joystick","x":0,"y":-1,"timestamp":time.time()},
+{"action":"joystick","x":-0.5,"y":-1,"timestamp":time.time()},
+{"action":"joystick","x":0,"y":-1,"timestamp":time.time()},
+{"action":"joystick","x":0.5,"y":-1,"timestamp":time.time()},
+{"action":"joystick","x":0,"y":-1,"timestamp":time.time()},
+{"action":"joystick","x":0,"y":0,"timestamp":time.time()}]
 async def echo(websocket):
 	while True:
-		x = round(random.uniform(1,-1),1)
-		y = round(random.uniform(1,-1),1)
-		message_joystick = {"action":"joystick","x":x,"y":y,"timestamp":time.time()}
-		message = json.dumps(message_joystick)
-		#message = (x,y)
-		await websocket.send((message))
-		print(f"sent coordinates {message}to client")
-		time.sleep(5)
+		#x = round(random.uniform(1,-1),1)
+		#y = round(random.uniform(1,-1),1)
+		for messages in message_joystick:
+			message = json.dumps(messages)
+			#message = (x,y)
+			await websocket.send((message))
+			print(f"sent coordinates {message}to client")
+			time.sleep(2)
 async def main():
 	async with websockets.serve(echo, "localhost", 8000):
 		await asyncio.Future() #makes it run forever
--- Raspberry-PI/websocket/websocket_to_arduino.py
+++ Raspberry-PI/websocket/websocket_to_arduino.py
@@ -3,9 +3,10 @@ import json
 import asyncio
 import time
 import serial
+import math
 
 #if serial fail try to change it to /dev/ttyACM0 /dev/ttyUSB1 or /dev/ttyUSB0
-ser = serial.Serial("/dev/ttyACM0", 115200, timeout=1)
+ser = serial.Serial("/dev/ttyUSB0", 115200, timeout=1)
 
 ser.setDTR(False)
 time.sleep(1)
@@ -17,18 +18,49 @@ websocket_server = "wss://ims-group4-backend.azurewebsites.net/ws/mower"
 mock_server = "ws://localhost:8000"
 
 
+def trashpanda_run(x,y):
+	item_length = math.sqrt(pow(x,2) + pow(y, 2))
+	big_speed = 180
+	direction = 1
+	if y < 0:
+		direction = -1
+	leftMotorDifferential = abs((x + 1) / 2)
+	rightMotorDifferential = (1 - leftMotorDifferential)
+	leftMotorDifferential *= direction
+	rightMotorDifferential *= direction
+	speed = int(big_speed * item_length)
+	rightMotor = int(rightMotorDifferential * speed)
+	leftMotor = int(leftMotorDifferential * speed)
+	motorSpeeds = [rightMotor,leftMotor]
+	return motorSpeeds
+	
+	
 # Currently having issue with recurring messages, can only view every other message sent by the server
 # This will be tested with the liveserver when the app is ready to send data to the mower.
 def websocket_client():
-	with connect(mock_server) as websocket:
+	diff_speed = 0
+	with connect(websocket_server) as websocket:
 		while True:
 			message_recv = websocket.recv()
 			print(f"Received: {message_recv}")
 			data = json.loads(message_recv)
 			data_action = data["action"]
+			
 			if data_action == "joystick":
-				print("wiener butt")
-			elif data_action == "autonamous":
+				x = data["x"]
+				y = data["y"]
+				
+				motorSpeeds = trashpanda_run(x,y)
+				totSpeed = motorSpeeds[0] + motorSpeeds[1]
+				send_data = f'1,{motorSpeeds[0]},{motorSpeeds[1]}'
+				print("send_data:",str(send_data))
+				ser.write(send_data.encode('utf-8'))
+				
+				#recieved_data= ser.readline()
+				#print(recieved_date)
+				print("LeftSpeed = {}, RightSpeed = {}".format(motorSpeeds[0],motorSpeeds[1]))
+				
+			elif data_action == "autonomous":
 				print("butt wiener")
 			else:
 				print("chilla")
@@ -36,6 +68,7 @@ def websocket_client():
 			#bytes(send_data,'utf-8')
 			#ser.write(send_data.encode('utf-8'))
 			
+    
 websocket_client()
 
 
